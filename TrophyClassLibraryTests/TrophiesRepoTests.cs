fa04ff2da9f05d8abae370fa465e5e8f61f27f84
CHKiens <44415938+CHKiens@users.noreply.github.com>
2025-03-10 15:25:39 +0100

    Tilføjet Unit test filer manuelt til repo

---
+++ TrophyClassLibraryTests/TrophiesRepoTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrophyClassLibrary.TrophyClassLibrary;
+
+namespace TrophyClassLibraryTests.TrophyClassLibraryTests
+{
+    [TestClass()]
+    public class TrophiesRepoTests
+    {
+        private TrophiesRepo _repo;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _repo = new TrophiesRepo();
+            _repo.Add(new Trophy { Competition = "Comp F", Year = 2024 });
+            _repo.Add(new Trophy { Competition = "Comp G", Year = 2019 });
+            _repo.Add(new Trophy { Competition = "Comp H", Year = 2017 });
+            _repo.Add(new Trophy { Competition = "Comp I", Year = 2021 });
+        }
+
+        [TestMethod()]
+        public void GetTrophyByIdTest()
+        {
+            Trophy? trophy = _repo.GetTrophyById(1);
+            Assert.AreEqual("Comp A", trophy?.Competition);
+            Assert.AreEqual(2018, trophy?.Year);
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Trophy? trophy = _repo.GetTrophyById(10);
+            Assert.IsNull(trophy);
+            _repo.Add(new Trophy { Competition = "Comp J", Year = 2015 });
+            trophy = _repo.GetTrophyById(10);
+            Assert.AreEqual("Comp J", trophy?.Competition);
+            Assert.AreEqual(2015, trophy?.Year);
+        }
+
+        [TestMethod()]
+        public void GetTrophies_FilterByYearBeforeTest()
+        {
+            IEnumerable<Trophy> trophies = _repo.GetTrophies(trophyYearBefore: 2019);
+            Assert.AreEqual(3, trophies.Count());
+        }
+
+        [TestMethod()]
+        public void GetTrophies_FilterByYearDescTest()
+        {
+            IEnumerable<Trophy> trophies = _repo.GetTrophies(orderBy: "year_desc");
+            Assert.AreEqual(9, trophies.Count());
+            Assert.AreEqual(2024, trophies.First().Year);
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Trophy? trophy = _repo.GetTrophyById(1);
+            Assert.AreEqual("Comp A", trophy?.Competition);
+            Assert.AreEqual(2018, trophy?.Year);
+            trophy.Competition = "Comp Z";
+            trophy.Year = 2025;
+            _repo.Update(1, trophy);
+            trophy = _repo.GetTrophyById(1);
+            Assert.AreEqual("Comp Z", trophy?.Competition);
+            Assert.AreEqual(2025, trophy?.Year);
+        }
+
+    }
+}
---
+++ TrophyClassLibraryTests/TrophyTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrophyClassLibrary.TrophyClassLibrary;
+
+namespace TrophyClassLibraryTests.TrophyClassLibraryTests
+{
+    [TestClass()]
+    public class TrophyTests
+    {
+        private readonly Trophy _trophy = new(1, "Comp X", 1980);
+
+        [TestMethod()]
+        public void TrophyTest()
+        {
+            Assert.AreEqual(1, _trophy.Id);
+            Assert.AreEqual("Comp X", _trophy.Competition);
+            Assert.AreEqual(1980, _trophy.Year);
+
+            Assert.ThrowsException<ArgumentNullException>(() => _trophy.Competition = null);
+            Assert.ThrowsException<ArgumentException>(() => _trophy.Competition = "AB");
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _trophy.Year = 1969);
+        }
+
+        [TestMethod()]
+        public void NoArgConstructorTest()
+        {
+            Trophy trophy = new Trophy();
+            Assert.AreEqual(0, trophy.Id);
+            Assert.AreEqual("Unknown", trophy.Competition);
+            Assert.AreEqual(2000, trophy.Year);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.AreEqual("Trophy: 1, Comp X - 1980", _trophy.ToString());
+        }
+    }
+}
